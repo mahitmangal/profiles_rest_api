a4e733a35906d71c192fa321e409aeecf771bacf
Mahit <ext-shinu.thomas@medlife.com>
2019-10-23 12:51:52 +0530

    viewSet added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response 
 from rest_framework import status
-
+from rest_framework import viewsets
 from profiles_api import serializers
 
 
@@ -52,3 +52,14 @@ class HelloApiView(APIView):
 
         return Response({'method': 'DELETE'})
 
+class HelloViewSet(viewsets.ViewSet):
+    """Test Api view set """
+
+    def list(self,requset):
+        """returns hello message"""
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code',
+        ]
+        return Response({'message':'Hello!','a_viewset':a_viewset})
